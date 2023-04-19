8d0e9aaa703e779d932ba3ec2f9d4f1049150701
unknown <magsudrm@code.edu.az>
2023-04-19 04:24:17 +0400

    hesbisey

---
+++ HomeworkPustok/Pustok/Areas/manage/Controllers/AuthorController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pustok.DAL;
+
+namespace Pustok.Areas.manage.Controllers
+{
+    [Area("manage")]
+
+    public class AuthorController : Controller
+    {
+        private readonly PustokDbContext _context;
+
+        public AuthorController(PustokDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index(int page = 1)
+        {
+            var model = _context.Authors
+                .Include(x => x.Books)
+                .Skip((page - 1) * 3)
+                .Take(3).ToList();
+            return View(model);
+        }
+    }
+}
---
+++ HomeworkPustok/Pustok/wwwroot/Assets/js/main.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    $(document).on("click", '.open-book-model', function (e) {
+
+        e.preventDefault();
+        var url = $(this).attr("href");
+
+        fetch(url)
+            .then(response => response.text())
+            .then(modalHtml => {
+                $("#quickModal .modal-dialog").html(modalHtml)
+            });
+
+
+        $("#quickModal").modal("show")
+    })
+    $(document).on("click", '.add-basket', function (e) {
+
+        e.preventDefault();
+        var url = $(this).attr("href");
+        let basketNumber = $(".text-number")[0];
+        fetch(url)
+            .then(response => response.text())
+            .then(html => $("#basket-cart").html(html))
+
+    })
+})
